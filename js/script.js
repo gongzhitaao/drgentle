30da05459ddd57e6a9c23ca3fe9a8cab0fac0a42
Zhitao Gong <zhitaao.gong@gmail.com>
2014-10-13 10:34:56 -0500

    formatting

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 (function($, undefined) {
 
 function fill_pub_list() {
+
   $.getJSON('pub.json', function(data) {
+
     $('#pub-cnt').text(data.length);
 
     var i, tmp, a, b, c, list = $('<div/>');
     for (i = 0; i < data.length; ++i) {
       c = $('<div/>', {'class': 'row'});
 
-      a = $('<div/>', {'class': 'col-lg-3'});
+      a = $('<div/>', {
+        'class': 'col-lg-3 text-right',
+        'style': 'padding-top: 20px'});
 
-      b = $('<h4/>', {'style': 'margin-top: 30px'}).text(data[i].publisher);
+      b = $('<em/>').text(data[i].journal);
+      a.append($('<p/>').append(b));
+
+      b = $('<p/>', {'style': 'font-size: medium'})
+          .text((data[i].volume ? "Volume " + data[i].volume : "") +
+                (data[i].number ? ", Issue" + data[i].number : ""));
       a.append(b);
 
-      b = $('<p/>').text(data[i].location);
-      a.append(b);
+      if (data[i].pages) {
+        b = $('<p/>', {'style': 'font-size: medium'})
+            .text("Pages " + data[i].pages);
+        a.append(b);
+      }
 
       c.append(a);
 
       a = $('<div/>', {'class': 'col-lg-9'});
 
       b = $('<h3/>').text(data[i].title);
+      a.append(b);
+
+      b = $('<em/>', {'style': 'font-size: medium'}).text(data[i].author);
       a.append(b);
 
       b = $('<p/>').text(data[i].abstract);
